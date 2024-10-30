52d6d3d8ef7e1d698fd031e6fe0c0a32dd80b04f
Mattias.Astrom <mattias.astrom@hotmail.com>
2024-10-30 16:36:49 +0100

    added so main goes into bankApp.Run()

--- BankApp.cs
+++ BankApp.cs
@@ -8,5 +8,14 @@ namespace BankApplication
 {
     internal class BankApp
     {
+        public BankApp()
+        {
+            //setup
+            //TODO : Load accounts from file.
+        }
+        public void Run()
+        {
+            //Does nothing yet.
+        }
     }
 }
