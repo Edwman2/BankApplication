e72ab532df0edcd82cd8759d0d5d99ad8c7cc587
Edwman2 <122057797+Edwman2@users.noreply.github.com>
2024-11-04 00:06:53 +0100

    Det jag hittills kommit med

--- BankApp.cs
+++ BankApp.cs
@@ -8,5 +8,6 @@ namespace BankApplication
 {
     internal class BankApp
     {
+        
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,20 @@ namespace BankApplication
     {
         static void Main(string[] args)
         {
+            TransactionManager transaction1 = new TransactionManager(1001, 1002, 500, DateTime.Now, TransactionManager.AddTransactionID(),615);
+            TransactionManager transaction2 = new TransactionManager(1003, 1006, 601, DateTime.Now, TransactionManager.AddTransactionID(), 615);
+            TransactionManager transaction3 = new TransactionManager(1034, 1002, 5850, DateTime.Now, TransactionManager.AddTransactionID(), 6000);
+            transaction1.DisplayTransactionInfo();
+            transaction1.AddFee();
+            transaction1.CheckBalance();
+            transaction2.DisplayTransactionInfo();
+            transaction2.AddFee();
+            transaction2.CheckBalance();
+            transaction3.DisplayTransactionInfo();
+            
+            transaction3.CheckBalance();
+
+            Console.ReadKey();
         }
     }
 }
