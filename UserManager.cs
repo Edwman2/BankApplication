6d9b660bdaeece1c84020b64f2e26282a20532ec
jakob12291 <hhalo007.jd@gmail.com>
2024-11-04 18:11:39 +0100

    Base class

---
+++ UserManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApplication
+{
+    internal class UserManager
+    {
+    }
+}
+public class User
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public int FailedLoginAttempts { get; set; }
+
+    public User(string username, string password)
+    {
+        Username = username;
+        Password = password;
+        FailedLoginAttempts = 0;
+    }
+}
