85ed908ca5d7fa217df92758b46283e8e359efff
Edwman2 <122057797+Edwman2@users.noreply.github.com>
2024-11-05 13:24:02 +0100

    Raderade onödig kod och synkade

--- Program.cs
+++ Program.cs
@@ -11,20 +11,7 @@ namespace BankApplication
     {
         static void Main(string[] args)
         {
-            TransactionManager transaction1 = new TransactionManager(1001, 1002, 500, DateTime.Now, TransactionManager.AddTransactionID(),615);
-            TransactionManager transaction2 = new TransactionManager(1003, 1006, 601, DateTime.Now, TransactionManager.AddTransactionID(), 615);
-            TransactionManager transaction3 = new TransactionManager(1034, 1002, 5850, DateTime.Now, TransactionManager.AddTransactionID(), 6000);
-            transaction1.DisplayTransactionInfo();
-            transaction1.AddFee();
-            transaction1.CheckBalance();
-            transaction2.DisplayTransactionInfo();
-            transaction2.AddFee();
-            transaction2.CheckBalance();
-            transaction3.DisplayTransactionInfo();
-            
-            transaction3.CheckBalance();
-
-            Console.ReadKey();
+          
         }
     }
 }
--- TransactionManager.cs
+++ TransactionManager.cs
@@ -9,67 +9,9 @@ namespace BankApplication
 
     internal class TransactionManager
     {
-        public int WithdrawFrom { get; set; }
-        public int DepositTo { get; set; }
-        public decimal Balance { get; set; }
-        public decimal Amount { get; set; }
-        public DateTime TransactionDate = DateTime.Now;
-        public int TransactionID { get; set; }
-        public decimal TransactionFee { get; set; }
+       internal int WithdrawFrom { get; set; }
+        internal int Reciever { get; set; }
 
-
-        internal TransactionManager(int withdrawFrom, int depositTo, decimal amount, DateTime date, int transactionID, decimal balance)
-        {
-            WithdrawFrom = withdrawFrom;
-            DepositTo = depositTo;
-            Amount = amount;
-            TransactionDate = date;
-            TransactionID = transactionID;
-            TransactionFee = 15;
-            Balance = balance;
-        }
-
-
-        
-        internal static int AddTransactionID()
-        {
-            return new Random().Next(100, 1000000);
-        }
-        internal void AddFee()
-        {
-            if (Amount > 5000)
-            {
-                Amount = Amount;
-            }
-            else
-            {
-                Amount = Amount + TransactionFee;
-                Console.WriteLine("Transationfee has been added: " + TransactionFee +"kr");
-            }
-
-        }
-        internal void CheckBalance()
-        {
-            if (Amount > Balance)
-            {
-                Console.WriteLine("you have insufficient balance");
-            }
-            else
-            {
-                Console.WriteLine("Transaction successful! You have transfered :" + Amount + "kr");
-            }
-        }
-
-        internal void DisplayTransactionInfo()
-        {
-            Console.WriteLine("Transaction ID: " + TransactionID);
-            Console.WriteLine("Withdraw From: " + WithdrawFrom);
-            Console.WriteLine("Deposit To: " + DepositTo);
-            Console.WriteLine("Amount: " + Amount);
-            Console.WriteLine("Date: " + TransactionDate);
-            Console.WriteLine("Balance: " + Balance);
-            
-        }
 
     }   
 
