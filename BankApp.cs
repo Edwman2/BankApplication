44c401007f7d264eb51f7c161eb8bec3472fe0d8
Mattias.Astrom <mattias.astrom@hotmail.com>
2024-11-11 11:54:58 +0100

    removed test

--- BankApp.cs
+++ BankApp.cs
@@ -9,26 +9,15 @@ namespace BankApplication
 {
     internal class BankApp
     {
-        CurrencyManager currencyManager;
         
         public BankApp()
         {
             //Setup
-            currencyManager = new CurrencyManager();
         }
 
         public void Run()
         {
             //Does nothing yet.
-            var temp = currencyManager.GetExchangeRates();
-
-            //loop through temp and write out the exchange rates and key.
-            foreach (var item in temp)
-            {
-                Console.WriteLine("Key: " + item.Key.AbbreviatedNameOfCurrency + " Value: " + item.Value);
-            }
-
-            Console.ReadLine();
         }
     }
 }
